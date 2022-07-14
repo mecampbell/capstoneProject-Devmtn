a08c8bc8589fca79f0e9f4c21d28245ffef9d3a9
Michael Campbell <michaelmichael323@hotmail.com>
2022-07-14 15:48:45 -0600

    backend working, made some updates to html css and js

--- public/homepage/script.js
+++ public/homepage/script.js
@@ -1,13 +1,18 @@
-const startBtn = document.getElementById('start-btn')
-const nextButton = document.getElementById('next-btn')
-const resultsButton = document.getElementById('results-btn')
-const questionContainerElement = document.getElementById('question-container')
-const questionElement = document.getElementById('question')
-const answerButtonsElement = document.getElementById('answer-buttons')
+const startBtn = document.getElementById('start-btn');
+const nextButton = document.getElementById('next-btn');
+const resultsButton = document.getElementById('results-btn');
+// const noButton = document.getElementById('//*[@id="answer-buttons"]/button[2]');
+const questionContainerElement = document.getElementById('question-container');
+const questionElement = document.getElementById('question');
+const answerButtonsElement = document.getElementById('answer-buttons');
 const resultContainer = document.getElementById('result-container');
 const containerElement = document.getElementById('container');
+// const guardianContainer = document.getElementById("guardian-container");
+// const sentinelContainer = document.getElementById("sentinel-container");
+// const consularContainer = document.getElementById("consular-container");
+// const nonJediContainer = document.getElementById("non-jedi-container");
 
-let currentQuestionIndex, guardian, sentinel, consular
+let currentQuestionIndex
 
 function startGame() {
   startBtn.classList.add('hide');
@@ -16,17 +21,24 @@ function startGame() {
   setNextQuestion();
 };
 
+// function nonJedi() {
+//   containerElement.classList.add('hide');
+//   resultsButton.classList.add('hide');
+//   resultContainer.classList.remove('hide');
+//   nonJediContainer.classList.remove('hide');
+// }
+
 function setNextQuestion() {
   resetState()
   showQuestion(questions[currentQuestionIndex])
-}
+};
 
 function resetState() {
   nextButton.classList.add('hide')
   while (answerButtonsElement.firstChild) {
     answerButtonsElement.removeChild(answerButtonsElement.firstChild)
   }
-}
+};
 
 function showQuestion(question) {
   questionElement.innerText = question.question
@@ -37,7 +49,7 @@ function showQuestion(question) {
     button.addEventListener('click', selectAnswer)
     answerButtonsElement.appendChild(button)
   })
-}
+};
 
 function selectAnswer(e) {
   const selectedButton = e.target
@@ -48,60 +60,74 @@ function selectAnswer(e) {
     resultsButton.classList.remove('hide')
     startBtn.classList.add('hide')
   }
-}
+};
 
 const showResult = () => {
   containerElement.classList.add('hide');
   resultsButton.classList.add('hide');
   resultContainer.classList.remove('hide');
-  resultContainer.classList.add('guardian');
-}
 
-startBtn.addEventListener('click', startGame)
+  if (questions.answer[1] === true) {
+    nonJediContainer.classList.remove('hide');
+  }
+
+  if (questions.answer[0] = 2) {
+    guardianContainer.classList.remove('hide');
+  }
+  if (questions.answer[1] = 2) {
+    sentinelContainer.classList.remove('hide');
+  }
+  if (questions.answer[2] = 2) {
+    consularContainer.classList.remove('hide');
+  }
+};
+
+
+startBtn.addEventListener('click', startGame);
 nextButton.addEventListener('click', () => {
   currentQuestionIndex++
   setNextQuestion()
-})
-resultsButton.addEventListener('click', showResult)
+});
+resultsButton.addEventListener('click', showResult);
 
 const questions = [
     {
         question: "Are you ready to be a Jedi?",
         answers: [
-            { text: 'Yes', correct: true },
-            { text: 'No', correct: false },
+            { text: 'Yes'},
+            { text: 'No'},
         ]
     },
     {
         question: "A woman and her small child are beset by a desperate-looking group of thugs. They are menacing her with weapons and she screams to you for help. What do you do?",
         answers: [
-            { text: 'Help them flee', guardian },
-            { text: 'Attack the thugs', sentinel },
-            { text: 'Stop the thugs and find out why they are attacking her.', consular }
+            { text: 'Help them flee'},
+            { text: 'Attack the thugs'},
+            { text: 'Stop the thugs and find out why they are attacking her.'}
         ]
     },
     {
         question: "You are in combat with a Dark Jedi allied with the Sith. There is a pause in the combat. What do you do?",
         answers: [
-            { text: 'Attack him again.', guardian },
-            { text: 'Find out why he turned to the dark side and try to turn him.', sentinel },
-            { text: 'Try to see a weakness in his technique.', consular }
+            { text: 'Attack him again.'},
+            { text: 'Find out why he turned to the dark side and try to turn him.'},
+            { text: 'Try to see a weakness in his technique.'}
         ]
     },
     {
         question: "There is a locked door and your goal lies on the other side. What do you do?",
         answers: [
-            { text: 'Smash the door down.', guardian },
-            { text: 'Try to pick the lock.', sentinel },
-            { text: 'Knock.', consular }
+            { text: 'Smash the door down.'},
+            { text: 'Try to pick the lock.'},
+            { text: 'Knock.'}
         ]
     },
     {
         question: "I have a feeling about what you would be best at. But first, the final question. You are the head of an Enclave on a contested world. The Dark Jedi have infiltrated and are causing unrest across the planet. What do you do?",
         answers: [
-            { text: 'Hunt them down.', guardian },
-            { text: 'Try to lure them out into a trap.', sentinel },
-            { text: 'Coordinate with the planetary government to identify the infiltrators.', consular }
+            { text: 'Hunt them down.'},
+            { text: 'Try to lure them out into a trap.'},
+            { text: 'Coordinate with the planetary government to identify the infiltrators.'}
         ]
     }
 ]
