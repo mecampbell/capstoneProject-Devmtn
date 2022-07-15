638fa8185b3575ec954eb432f280283e76120d26
Michael Campbell <michaelmichael323@hotmail.com>
2022-07-14 19:05:13 -0600

    adding an app use

--- server.js
+++ server.js
@@ -5,18 +5,19 @@ const path = require("path");
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, '/public')));
 
 app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "/public/homepage/index.html"));
 });
 
-app.get("/styles", function(req, res) {
-    res.sendFile(path.join(__dirname, "/public/homepage/styles.css"));
-});
+// app.get("/styles", function(req, res) {
+//     res.sendFile(path.join(__dirname, "/public/homepage/styles.css"));
+// });
 
-app.get("/js", function(req, res) {
-    res.sendFile(path.join(__dirname, "/public/homepage/script.js"));
-});
+// app.get("/js", function(req, res) {
+//     res.sendFile(path.join(__dirname, "/public/homepage/script.js"));
+// });
 
 const port = process.env.PORT || 5555;
 
