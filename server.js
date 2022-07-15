4a8581dec954dd11a454b26bc2dfe00e0412d342
Michael Campbell <michaelmichael323@hotmail.com>
2022-07-14 19:10:51 -0600

    fixing app use

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require("path");
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.static(path.join(__dirname, '/public/homepage')));
 
-app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "/public/homepage/index.html"));
-});
+// app.get("/", function(req, res) {
+//     res.sendFile(path.join(__dirname, "/public/homepage/index.html"));
+// });
 
 // app.get("/styles", function(req, res) {
 //     res.sendFile(path.join(__dirname, "/public/homepage/styles.css"));
